be8de35bd67fc794e05c29ba7bc216d628ddab09
Brandon Carmody <BmcPrimary@yahoo.com>
2023-04-01 17:27:12 -0600

    Start config

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -1,11 +1,10 @@
--- customize mason plugins
 return {
   -- use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      -- ensure_installed = { "lua_ls" },
+      ensure_installed = { "lua_ls" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
@@ -13,7 +12,7 @@ return {
     "jay-babu/mason-null-ls.nvim",
     -- overrides `require("mason-null-ls").setup(...)`
     opts = {
-      -- ensure_installed = { "prettier", "stylua" },
+      ensure_installed = { "prettier", "stylua" },
     },
   },
   {

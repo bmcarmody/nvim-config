e0ab4a9a884a9fecaca67f1673aa3184b0b3c4a1
Brandon Carmody <BmcPrimary@yahoo.com>
2023-04-01 19:19:44 -0600

    Configure splash screen

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,7 @@
 return {
   "nvim-treesitter/nvim-treesitter",
   opts = {
-    -- ensure_installed = { "lua" },
+    ensure_installed = { "lua", "javascript", "typescript", "vim" },
+    auto_install = true,
   },
 }

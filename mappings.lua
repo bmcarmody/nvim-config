315772cbe2567fa307b00fcabc61cfac73a03fa4
Brandon Carmody <BmcPrimary@yahoo.com>
2023-04-23 00:50:30 -0600

    Add remap to copilot accept suggestion

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -24,5 +26,8 @@ return {
   t = {
     -- setting a mapping to false will disable it
     -- ["<esc>"] = false,
+  },
+  i = {
+    ["<C-J>"] = { "copilot#Accept('<CR>')", desc = "Accept Copilot suggestion", expr = true },
   },
 }
--- options.lua
+++ options.lua
@@ -16,6 +16,8 @@ return {
     diagnostics_mode = 3, -- set the visibility of diagnostics in the UI (0=off, 1=only show in status line, 2=virtual text off, 3=all on)
     icons_enabled = true, -- disable icons in the UI (disable if no nerd font is available, requires :PackerSync after changing)
     ui_notifications_enabled = true, -- disable notifications when toggling UI elements
+    copilot_no_tab_map = true, -- disable tab mapping in copilot
+    copilot_assume_mapped = true, -- assume that copilot is already mapped
   },
 }
 -- If you need more control, you can use the function()...end notation

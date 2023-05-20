abac5bc9d45b1c823916ee7f7ffd4d5f09b9e01b
Brandon Carmody <BmcPrimary@yahoo.com>
2023-05-20 13:38:37 -0600

    Add move block command

--- mappings.lua
+++ mappings.lua
@@ -1,15 +1,19 @@
 return {
   n = {
-    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
+    ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New buffer" },
     ["<leader>bD"] = {
       function()
         require("astronvim.utils.status").heirline.buffer_picker(
           function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
         )
       end,
-      desc = "Pick to close",
+      desc = "Pick buffer to close",
     },
     ["<leader>b"] = { name = "Buffers" },
+  },
+  x = {
+    ["J"] = { ":m '>+1<CR>gv=gv", desc = "Move line down" },
+    ["K"] = { ":m '<-2<CR>gv=gv", desc = "Move line up" },
   },
   i = {
     ["<C-j>"] = {

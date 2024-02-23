5dcdea37dbd2cc63a37449db5aee68a92263058f
Brandon Carmody <BmcPrimary@yahoo.com>
2024-02-22 20:23:10 -0800

    Remove unused plugins

--- plugins/todo-comments.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "folke/todo-comments.nvim",
-  dependencies = { "nvim-lua/plenary.nvim" },
-  opts = {},
-  event = "User AstroFile",
-  keys = {
-    { "<leader>t", "<cmd>TodoTelescope<cr>", desc = "Open TODOs in Telescope" },
-  },
-}
